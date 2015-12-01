19b1e3b66d544b43c71a19dd3ab443a860c24a44
SilencedBacon <fredjames007@me.com>
2015-12-01 20:44:24 +0000

    Trying to get the code up

---
+++ ADPROC/src/adproc/PipeType1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adproc;
+
+/**
+ * Created by Fred on 01/12/15.
+ */
+public class PipeType1 extends Pipe {
+    public PipeType1(Boolean pipeOuterReinforcement, int pipePlasticGrade, int pipecolour, double pipeLength, double pipeOuterRadius, double piperInnerRadius, Boolean pipeChemicalResist, Boolean pipeInsulation) {
+        super(pipeOuterReinforcement, pipePlasticGrade, pipecolour, pipeLength, pipeOuterRadius, piperInnerRadius, pipeChemicalResist, pipeInsulation);
+    }
+}
